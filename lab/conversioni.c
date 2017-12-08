d554d0729c23a3688e1b40a0e8d60cec5f5bea94
anesemarco <34371581+anesemarco@users.noreply.github.com>
2017-12-08 14:51:07 +0100

    Update conversioni.c

--- lab/conversioni.c
+++ lab/conversioni.c
@@ -1,6 +1,8 @@
 /* 1.6 CONVERSIONI IN SERIE */
 
-/* Scrivere un programma che acquisisce una sequenza di numeri interi relativi, a priori di lunghezza ignota e terminata quando l'utente inserisce 0, e per ciascuno di essi visualizza la sua rappresentazione in base 2, notazione modulo e segno. Per ogni conversione il programma visualizza datoiniziale = codifica\n. */
+/* Scrivere un programma che acquisisce una sequenza di numeri interi relativi, a priori di lunghezza ignota 
+e terminata quando l'utente inserisce 0, e per ciascuno di essi visualizza la sua rappresentazione in base 2, 
+notazione modulo e segno. Per ogni conversione il programma visualizza datoiniziale = codifica\n. */
 
 #include <stdio.h>
 #define BASE 2
