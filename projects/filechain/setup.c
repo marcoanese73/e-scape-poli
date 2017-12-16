5c910aa8165228e812a6582e2381e5f05cdbcf3f
anesemarco <anesemarco@gmail.com>
2017-12-16 18:13:15 +0100

    Chiamate ai sottoprogrammi
    Le funzioni di inizializzazione e conclusione del setup, creazione di directory e file (.ldg, .bin e adesso anche .c) sono state destinate ai nuovi sottoprogrammi di libreria.

--- projects/filechain/setup.c
+++ projects/filechain/setup.c
@@ -1,210 +1,67 @@
-/* SETUP file:
- * Crea cartelle di simulazione e i relativi registri (ledger.ldg e status.ldg) per ogni cartella di simulazione
- */
+/* ================================================= *
+ * SETUP file:-------------------------------------- *
+ * Creates folders for simulation (nodes)----------- *
+ * and related registers (ledger.ldg and status.bin) *
+ * ================================================= */
 
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include "./setlib.h"
 
-#define LEDGER_FILE_NAME "./simulation/ledger.ldg"
-#define LEDGER_DIR_NAME "./simulation/"
-#define MATRIX_DIR_NAME "./simulation/matrix/"
-#define NODE_1_DIR_NAME "./simulation/matrix/node-1/"
+#define LEDGER_FILE_NAME   "./simulation/ledger.ldg"
+#define LEDGER_DIR_NAME    "./simulation/"
+#define MATRIX_DIR_NAME    "./simulation/matrix/"
+#define NODE_1_DIR_NAME    "./simulation/matrix/node-1/"
 #define NODE_1_STATUS_NAME "./simulation/matrix/node-1/status-1.bin"
-#define NODE_2_DIR_NAME "./simulation/matrix/node-2/"
+#define NODE_2_DIR_NAME    "./simulation/matrix/node-2/"
 #define NODE_2_STATUS_NAME "./simulation/matrix/node-2/status-2.bin"
-#define NODE_3_DIR_NAME "./simulation/matrix/node-3/"
+#define NODE_3_DIR_NAME    "./simulation/matrix/node-3/"
 #define NODE_3_STATUS_NAME "./simulation/matrix/node-3/status-3.bin"
-#define NODE_4_DIR_NAME "./simulation/matrix/node-4/"
+#define NODE_4_DIR_NAME    "./simulation/matrix/node-4/"
 #define NODE_4_STATUS_NAME "./simulation/matrix/node-4/status-4.bin"
-#define NODE_5_DIR_NAME "./simulation/matrix/node-5/"
+#define NODE_5_DIR_NAME    "./simulation/matrix/node-5/"
 #define NODE_5_STATUS_NAME "./simulation/matrix/node-5/status-5.bin"
 #define NODE_1_LEDGER_NAME "./simulation/matrix/node-1/legder.ldg"
 #define NODE_2_LEDGER_NAME "./simulation/matrix/node-2/legder.ldg"
 #define NODE_3_LEDGER_NAME "./simulation/matrix/node-3/legder.ldg"
 #define NODE_4_LEDGER_NAME "./simulation/matrix/node-4/legder.ldg"
 #define NODE_5_LEDGER_NAME "./simulation/matrix/node-5/legder.ldg"
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-#define SLEEP 10000
+#define NODE_1_SIM_NAME    "./simulation/matrix/node-1/filechain.c"
+#define NODE_2_SIM_NAME    "./simulation/matrix/node-2/filechain.c"
+#define NODE_3_SIM_NAME    "./simulation/matrix/node-3/filechain.c"
+#define NODE_4_SIM_NAME    "./simulation/matrix/node-4/filechain.c"
+#define NODE_5_SIM_NAME    "./simulation/matrix/node-5/filechain.c"
 
 int main(int argc, char *argv[])
 {
-	struct stat st;
-	FILE *fp;
 	int errors;
 
-	printf("Eseguo il setup\n");
-	usleep(SLEEP);
-	errors = 0;
+	initSetup(&errors);
 
-	if(stat(LEDGER_DIR_NAME, &st) == -1) {
-		mkdir(LEDGER_DIR_NAME, 0700);
-		printf("Creata directory %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", LEDGER_DIR_NAME);
-	} else
-		printf("La directory %s esiste " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", LEDGER_DIR_NAME);
-	usleep(SLEEP);
+	createDir(LEDGER_DIR_NAME);
+	createLedger(LEDGER_FILE_NAME, &errors);
+	createDir(MATRIX_DIR_NAME);
+	createDir(NODE_1_DIR_NAME);
+	createStatus(NODE_1_STATUS_NAME, &errors);
+	createLedger(NODE_1_LEDGER_NAME, &errors);
+	createSim(NODE_1_SIM_NAME, &errors);
+	createDir(NODE_2_DIR_NAME);
+	createStatus(NODE_2_STATUS_NAME, &errors);
+	createLedger(NODE_2_LEDGER_NAME, &errors);
+	createSim(NODE_2_SIM_NAME, &errors);
+	createDir(NODE_3_DIR_NAME);
+	createStatus(NODE_3_STATUS_NAME, &errors);
+	createLedger(NODE_3_LEDGER_NAME, &errors);
+	createSim(NODE_3_SIM_NAME, &errors);
+	createDir(NODE_4_DIR_NAME);
+	createStatus(NODE_4_STATUS_NAME, &errors);
+	createLedger(NODE_4_LEDGER_NAME, &errors);
+	createSim(NODE_4_SIM_NAME, &errors);
+	createDir(NODE_5_DIR_NAME);
+	createStatus(NODE_5_STATUS_NAME, &errors);
+	createLedger(NODE_5_LEDGER_NAME, &errors);
+	createSim(NODE_5_SIM_NAME, &errors);
 
-	if(fp = fopen(LEDGER_FILE_NAME, "w")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", LEDGER_FILE_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Ledger\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", LEDGER_FILE_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-
-	if(stat(MATRIX_DIR_NAME, &st) == -1) {
-		mkdir(MATRIX_DIR_NAME, 0700);
-		printf("Creata directory %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", MATRIX_DIR_NAME);
-	} else
-		printf("La directory %s esiste " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", MATRIX_DIR_NAME);
-	usleep(SLEEP);
-
-	if(stat(NODE_1_DIR_NAME, &st) == -1) {
-		mkdir(NODE_1_DIR_NAME, 0700);
-		printf("Creata directory %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_1_DIR_NAME);
-	} else
-		printf("La directory %s esiste " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_1_DIR_NAME);
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_1_STATUS_NAME, "wb")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_1_STATUS_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Status (1)\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_1_STATUS_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_1_LEDGER_NAME, "w")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_1_LEDGER_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Ledger\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_1_LEDGER_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-
-	if(stat(NODE_2_DIR_NAME, &st) == -1) {
-		mkdir(NODE_2_DIR_NAME, 0700);
-		printf("Creata directory %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_2_DIR_NAME);
-	} else
-		printf("La directory %s esiste " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_2_DIR_NAME);
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_2_STATUS_NAME, "wb")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_2_STATUS_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Status (2)\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_2_STATUS_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-
-		if(fp = fopen(NODE_2_LEDGER_NAME, "w")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_2_LEDGER_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Ledger\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_2_LEDGER_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-	
-	if(stat(NODE_3_DIR_NAME, &st) == -1) {
-		mkdir(NODE_3_DIR_NAME, 0700);
-		printf("Creata directory %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_3_DIR_NAME);
-	} else
-		printf("La directory %s esiste " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_3_DIR_NAME);
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_3_STATUS_NAME, "wb")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_3_STATUS_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Status (3)\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_3_STATUS_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_3_LEDGER_NAME, "w")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_3_LEDGER_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Ledger\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_3_LEDGER_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-	
-	if(stat(NODE_4_DIR_NAME, &st) == -1) {
-		mkdir(NODE_4_DIR_NAME, 0700);
-		printf("Creata directory %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_4_DIR_NAME);
-	} else
-		printf("La directory %s esiste " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_4_DIR_NAME);
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_4_STATUS_NAME, "wb")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_4_STATUS_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Status (4)\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_4_STATUS_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_4_LEDGER_NAME, "w")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_4_LEDGER_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Ledger\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_4_LEDGER_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-	
-	if(stat(NODE_5_DIR_NAME, &st) == -1) {
-		mkdir(NODE_5_DIR_NAME, 0700);
-		printf("Creata directory %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_5_DIR_NAME);
-	} else
-		printf("La directory %s esiste " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_5_DIR_NAME);
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_5_STATUS_NAME, "wb")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_5_STATUS_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Status (5)\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_5_STATUS_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-
-	if(fp = fopen(NODE_5_LEDGER_NAME, "w")) {
-		printf("Creato file %s " ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n", NODE_5_LEDGER_NAME);
-		printDate(fp);
-		fprintf(fp, " Created Ledger\n");
-	} else {
-		printf("Errore nella creazione del file %s " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "\n", NODE_5_LEDGER_NAME);
-		errors++;
-	}
-	usleep(SLEEP);
-
-	printf("Setup completato ");
-	if(errors != 1)
-		printf("(%d errori)\n", errors);
-	else
-		printf("(%d errore)\n", errors);
+	endSetup(&errors);
 
 	return 0;
 }
