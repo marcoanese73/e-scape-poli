c3aaa40e1ac1b61f0521b9e1990657d8a994810a
elrriazluis <34798961+elrriazluis@users.noreply.github.com>
2018-01-04 14:26:31 +0000

    Delete randtxt.c

--- exercises/other/randtxt.c
+++
@@ -1,3 +0,0 @@
-oggi e' il 5 di settembre 2015.  per superare l’esame e' necessario prendere almeno 18 nella
-prova scritta.  Tale voto contribuisce, insieme alla valutazione di laboratorio, al voto
-finale
