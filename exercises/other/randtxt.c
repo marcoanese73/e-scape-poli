8b4255838ceb85ba8ae4c8bbf927bb7233ae8e3f
elrriazluis <34798961+elrriazluis@users.noreply.github.com>
2018-01-04 15:25:37 +0100

    Add files via upload

---
+++ exercises/other/randtxt.c
@@ -0,0 +1,3 @@
+oggi e' il 5 di settembre 2015.  per superare l’esame e' necessario prendere almeno 18 nella
+prova scritta.  Tale voto contribuisce, insieme alla valutazione di laboratorio, al voto
+finale
